76e4f318553de1baee0fd0b3a0d94802cc4ca672
Camilo Cossio Alzate <camilocossio@MacBook-Pro-de-Camilo.local>
2023-10-15 18:27:34 -0500

    Heuristic algorithm finished

--- model.py
+++ model.py
@@ -57,7 +57,7 @@ def assign_break_no_intersection(
         candidates_1:set,
         candidates_2:set,
         demand:np.array,
-        unable:set,
+        unavailable:set,
         before_lunch:bool,
         start_lunch:int,
         start_day:int
@@ -73,25 +73,22 @@ def assign_break_no_intersection(
     ordered_dict = dict(sorted(aux.items(), key=lambda item: item[1]))
     candidates = list(ordered_dict.keys())
     breaks.append(candidates[0])
-    unable = unable.union(set(list(range(candidates[0]-4,candidates[0]+5))))
+    unavailable = unavailable.union(set(list(range(candidates[0]-4,candidates[0]+5))))
     current_min_index = 0
     i = 1
-    # print('------------------------------')
-    # print(f'start_day = {start_day}')
-    # print(f'start_lunch = {start_lunch}')
-    # print(f'ordered_dict = {ordered_dict}')
-    # print('------------------------------')
-    while unable & switch != switch:
+    while unavailable & switch != switch:
         if i >= len(candidates):
             current_min_index += 1
             i = current_min_index + 1
         else:
             aux = abs(candidates[i] - candidates[current_min_index])
             if (aux >= 5) and (aux <= 9):
-                breaks.append(candidates[i])
-                unable = unable.union(set(list(range(candidates[i]-4,candidates[i]+5))))
+                breaks += [candidates[current_min_index],candidates[i]]
+                new_unavailable = set(list(range(candidates[i]-4,candidates[i]+5)))\
+                                .union(set(list(range(candidates[current_min_index]-4,candidates[current_min_index]+5))))
+                unavailable = unavailable.union(new_unavailable)
                 current_min_index = i
-                i += 1
+                i = current_min_index + 1
             else:
                 i += 1
     return breaks
@@ -103,29 +100,29 @@ def assign_breaks(start_day_schedule:list, lunch_schedule:list, demand:np.array,
         # Find the slots that cannot change due to the condition that employees must 
         # work minimum 1 hour (4 slots) at the beginning, at the end of the day, 
         # before and after lunch.
-        unable = set(
+        unavailable = set(
                     list(range(0,4)) + # Beginning of the day
                     list(range(aux-4,aux+10)) + # Before, during and after lunch
                     list(range(34,38)) # End of the day
                     )
         # Find the candidate stripes to be active pause at the beginning of the day and 
         # exclude those that cannot change
-        beginning_day = set(list(range(4,9))) - unable
+        beginning_day = set(list(range(4,9))) - unavailable
         # Find the candidate stripes to be active pause before the lunch and exclude 
         # those that cannot change
         if aux - 9 >= 0:
-            before_lunch = set(list(range(aux-9,aux-4))) - unable
+            before_lunch = set(list(range(aux-9,aux-4))) - unavailable
         else:
-            before_lunch = set(list(range(0,aux-4))) - unable
+            before_lunch = set(list(range(0,aux-4))) - unavailable
         # Find the candidate stripes to be active pause after the lunch and exclude 
         # those that cannot change
         if aux + 15 <= 38:
-            after_lunch = set(list(range(aux+10,aux+15))) - unable
+            after_lunch = set(list(range(aux+10,aux+15))) - unavailable
         else:
-            after_lunch = set(list(range(aux+10,38))) - unable
+            after_lunch = set(list(range(aux+10,38))) - unavailable
         # Find the candidate stripes to be active pause at the end of the day and 
         # exclude those that cannot change
-        ending_day = set(list(range(29,34))) - unable
+        ending_day = set(list(range(29,34))) - unavailable
         # Evaluate if there are common elements in beginning_day and before_lunch
         if beginning_day & before_lunch:
             brk = assign_break_intersection(beginning_day, before_lunch, demand, start_day_schedule[i])
@@ -134,7 +131,7 @@ def assign_breaks(start_day_schedule:list, lunch_schedule:list, demand:np.array,
             brk = assign_break_no_intersection(beginning_day,
                                                 before_lunch,
                                                 demand,
-                                                unable,
+                                                unavailable,
                                                 True,
                                                 lunch_schedule[i],
                                                 start_day_schedule[i])
@@ -150,7 +147,7 @@ def assign_breaks(start_day_schedule:list, lunch_schedule:list, demand:np.array,
             brk = assign_break_no_intersection(after_lunch,
                                                 ending_day,
                                                 demand,
-                                                unable,
+                                                unavailable,
                                                 False,
                                                 lunch_schedule[i],
                                                 start_day_schedule[i])
@@ -174,19 +171,14 @@ def calculate_shortfall(matrix, demand):
     """
     # Ensure that the demand vector length matches the number of columns in the matrix
     assert matrix.shape[1] == len(demand), "Mismatch between matrix columns and demand vector length."
-
     # Calculate the number of workers working at each moment of time
     workers_working = (matrix == 1).sum(axis=0)  # Sum across rows for each column
-
     # Calculate the difference between demand and workers working
     difference = demand - workers_working
-
     # Sum only the positive differences
     total_shortfall = difference[difference > 0].sum()
-
     return total_shortfall
 
-#%%
 def main():
     # Folder where the script is located
     current_folder = os.path.dirname(os.path.abspath(__file__))
@@ -199,10 +191,10 @@ def main():
     best_schedule = None
     best_result = 1000000
     for i in range(10000):
-        print(i)
+        # print(i)
         schedule = pd.DataFrame()
         start_day_schedule = assign_start(n_workers)
-        start_day_schedule[0] = 1
+        # start_day_schedule[0] = 1
         # start_day_schedule = [3,8,0,2,1,4,6,5]
         lunch_schedule = assign_lunch(original_demand, n_workers)
         lunch_schedule[0] = 24
